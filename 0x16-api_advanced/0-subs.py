36c4ff03d48a9996ef07f8fb67093d1aa33af4b0
charles ezebuike <ezebuike.charles0@gmail.com>
2023-05-09 18:59:24 +0000

    Added Documentations

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = ("https://www.reddit.com/r/{}/about.json".format(subreddit))
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
